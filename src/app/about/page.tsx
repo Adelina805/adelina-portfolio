23dbb1f2034de65437f9d58bb0b206cda45c55b6
Adelina <adelinaballerina1@gmail.com>
2025-12-02 04:28:43 -0800

    adding about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,26 @@
-import { Link } from "lucide-react";
 import NextButton from "../NextButton";
 
 export default function AboutPage() {
   return (
-    <section className="relative min-h-[90vh] px-4">
+    <section className="relative h-full px-4 flex flex-col">
 
-    <h1
-      className="
-        font-bold
-        text-[40px]
-        line-through
-        hover:no-underline
-        transition-all
-        text-center
-        pt-[calc(clamp(1rem,4vw,2rem))]
-      "
-    >
-      ABOUT
+    {/* left column */}
+    // image
+
+    {/* right column */}
+    <h1 className="mt-10 font-bold tracking-tight leading-none text-[clamp(2rem,5vw,2.5rem)]">
+      Hello! my name is Adelina Martinez, a student, designer, and developer based in San Diego, CA
     </h1>
-    <p className="mt-5 mx-auto text-center text-base">
-      Student, Designer, Developer
+
+    <h1 className="mt-10 font-bold tracking-tight leading-none text-[clamp(2rem,5vw,2.5rem)]">
+      I like to make things for the internet.
+    </h1>
+
+    <p className="mt-10 mx-auto text-xl">
+      I am currently attending San Diego State University pursuing my B.A. in Computer Science with a minor in Website Design. I am passionate about UX/UI Design, Frontend Development, Web Development, Graphic Design, and/or anything that blends visually creative design with technical engineering and user interaction. I am a proud wearer of many hats and believe the most meaningful innovation happens at the intersection of diverse skill sets.
+      <br />
+      <br />
+      When I’m not studying, working, coding or designing, you can usually find me at the beach 🏖️ playing volleyball 🏐, surfing 🌊, or reading 📚. I also love watching movies 🎬, thrifting 👕, eating tasty food 🌮, exploring new places ✈️, and pondering 🤔.
     </p>
 
     <NextButton href="/experience" label="View my experiences >>" />
